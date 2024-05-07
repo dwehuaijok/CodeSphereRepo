0fc35c31d5596bf3221f99a1ea1d4633ae3c86ce
dwehuaijok <rsjcichardcolmn@hotmail.com>
2024-05-08 01:36:45 +0700

    update commit

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-function subarraySum(nums, k) {
+function groupAnagrams(strs) {
   const map = new Map();
-  map.set(0, 1);
-  let count = 0;
-  let sum = 0;
-  for (const num of nums) {
-    sum += num;
-    if (map.has(sum - k)) {
-      count += map.get(sum - k);
-    }
-    map.set(sum, (map.get(sum) || 0) + 1);
+  for (const str of strs) {
+    const sortedStr = str.split("").sort().join("");
+    if (!map.has(sortedStr)) map.set(sortedStr, []);
+    map.get(sortedStr).push(str);
   }
-  return count;
+  return [...map.values()];
 }
