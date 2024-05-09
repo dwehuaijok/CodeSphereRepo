1a18e16e8e9f456028b5a244bcfc879934663676
dwehuaijok <rsjcichardcolmn@hotmail.com>
2024-05-09 07:10:08 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-function findCircleNum(M) {
-  if (!M || M.length === 0) return 0;
-  const visited = new Array(M.length).fill(false);
-  let count = 0;
-  for (let i = 0; i < M.length; i++) {
-    if (!visited[i]) {
-      dfs(M, i, visited);
-      count++;
-    }
+function findPeakElement(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] < nums[mid + 1]) left = mid + 1;
+    else right = mid;
   }
-  return count;
-}
-function dfs(M, i, visited) {
-  visited[i] = true;
-  for (let j = 0; j < M.length; j++) {
-    if (M[i][j] === 1 && !visited[j]) {
-      dfs(M, j, visited);
-    }
-  }
+  return left;
 }
