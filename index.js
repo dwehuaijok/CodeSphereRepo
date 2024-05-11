c9f58c0679d601880b1bd4109ac89423fb369f06
dwehuaijok <rsjcichardcolmn@hotmail.com>
2024-05-12 02:23:36 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function numSquares(n) {
-  const dp = new Array(n + 1).fill(Infinity);
-  dp[0] = 0;
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j * j <= i; j++) {
-      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
-    }
-  }
-  return dp[n];
-}
+const randomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
